3b3a1cd13357ec88f78da33a10762d587a1efc90
choigeon96 <ck731@naver.com>
2023-08-22 07:24:46 +0900

    cors 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || DEFAULT_PORT;
 
 
 app.all("/*", function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://union.choigeon.com");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
