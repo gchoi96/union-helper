b643540af8c9d25dab254963a619645f9b10eb3c
choigeon96 <ck731@naver.com>
2023-08-22 08:18:05 +0900

    ga

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+}
+
 root.render(
   <React.StrictMode>
     <App />
